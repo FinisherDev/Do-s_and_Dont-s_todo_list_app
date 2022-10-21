db94a265f46c326fc4b5052823004b2fb0a4ad55
Chukwufumnanya <molokwujude002@gmail.com>
2022-10-22 00:16:33 +0100

    Bugs fixed and new hosting service chosen

--- Dos_and_Donts/settings.py
+++ Dos_and_Donts/settings.py
@@ -17,7 +17,7 @@ from datetime import timedelta
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age = 500)
-DATABASES['default'].update(db_from_env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,6 +109,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
