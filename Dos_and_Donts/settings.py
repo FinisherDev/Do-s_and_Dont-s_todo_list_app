6e348f93d5a54a3551e48b94bcb55ef382d7b08d
Chukwufumnanya <molokwujude002@gmail.com>
2022-10-22 00:12:44 +0100

    Bugs fixed and new hosting service chosen

--- Dos_and_Donts/settings.py
+++ Dos_and_Donts/settings.py
@@ -14,7 +14,7 @@ import socket
 from pathlib import Path
 from six.moves import configparser
 from datetime import timedelta
-import dj_database_url
+import dj-database-url
 
 db_from_env = dj_database_url.config(conn_max_age = 500)
 DATABASES['default'].update(db_from_env)
