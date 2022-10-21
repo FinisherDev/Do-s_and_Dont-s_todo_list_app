f8e27026e16da0f36627bdd1735c3dd98b257ea5
Chukwufumnanya <molokwujude002@gmail.com>
2022-10-21 23:36:28 +0100

    Bugs fixed and new hosting service chosen

--- Dos_and_Donts/settings.py
+++ Dos_and_Donts/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = config.get('security', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'evening-refuge-3851.herokuapp.com']
 
 AUTH_USER_MODEL = 'user.CustomUser'
 
