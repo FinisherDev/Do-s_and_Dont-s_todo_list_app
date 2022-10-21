68904e844812b765e6df19a241a75f1cfe0bcfdf
Chukwufumnanya <molokwujude002@gmail.com>
2022-10-21 23:49:03 +0100

    Bugs fixed and new hosting service chosen

--- frontend/src/tools/axiosAPI.js
+++ frontend/src/tools/axiosAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL =  "http://127.0.0.1:8000/api";
+const baseURL =  "http://evening-refuge-3851.herokuapp.com/api";
 
 const AuthService = axios.create({
     baseURL: baseURL,
