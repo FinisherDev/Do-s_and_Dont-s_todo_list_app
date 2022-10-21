40f5dd46b48aa9b1f67ea36e33bfa8994e28b905
Chukwufumnanya <molokwujude002@gmail.com>
2022-10-22 00:07:57 +0100

    Bugs fixed and new hosting service chosen

--- Dos_and_Donts/settings.py
+++ Dos_and_Donts/settings.py
@@ -14,7 +14,10 @@ import socket
 from pathlib import Path
 from six.moves import configparser
 from datetime import timedelta
+import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age = 500)
+DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
